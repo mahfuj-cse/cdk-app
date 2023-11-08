383cb48181479468d7e12c19c1533240ba608510
Abdullah Al Mahfuj <53569184+mahfuj-cse@users.noreply.github.com>
2023-11-08 19:37:32 +0600

    aws-lamda dynamodb apigateway added

--- lib/my_cdk_project-stack.ts
+++ lib/my_cdk_project-stack.ts
@@ -1,23 +1,67 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as constructs from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class MyCdkProjectStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: constructs.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, 'lambdaFunction', {
-      functionName: 'first-cdk-lambda',
-      code: new lambda.AssetCode('src'),
-      handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_16_X,
-      memorySize: 128
-    })
-
-    new s3.Bucket(this, 'FirstBucket', {
-      versioned: true
+    // DynamoDB Table
+    const dynamoTable = new dynamodb.Table(this, "MyDynamoDBTable", {
+      partitionKey: { name: "itemId", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Adjust as needed
+      billingMode: dynamodb.BillingMode.PROVISIONED, // Use Provisioned billing mode for Free Tier
+      readCapacity: 5, // Adjust based on your expected usage within Free Tier limits
+      writeCapacity: 5, // Adjust based on your expected usage within Free Tier limits
     });
+
+    // Lambda Functions
+    const getAllItemsFunction = new lambda.Function(
+      this,
+      "getAllItemsFunction",
+      {
+        runtime: lambda.Runtime.NODEJS_16_X,
+        handler: "get-all.handler",
+        code: lambda.Code.fromAsset("src"), // Replace with the correct code directory
+        environment: {
+          TABLE_NAME: dynamoTable.tableName,
+          PRIMARY_KEY: "itemId",
+        },
+      }
+    );
+
+    const createItemFunction = new lambda.Function(this, "createItemFunction", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "create.handler",
+      code: lambda.Code.fromAsset("src"), // Replace with the correct code directory
+      environment: {
+        TABLE_NAME: dynamoTable.tableName,
+        PRIMARY_KEY: "itemId",
+      },
+    });
+
+    // Grant Permissions
+    dynamoTable.grantReadWriteData(createItemFunction);
+    dynamoTable.grantReadData(getAllItemsFunction);
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, "MyApi", {
+      restApiName: "My API",
+    });
+
+    const rootResource = api.root;
+
+    // Integration with Lambda Functions
+    const getAllIntegration = new apigateway.LambdaIntegration(
+      getAllItemsFunction
+    );
+    rootResource.addMethod("GET", getAllIntegration);
+
+    const createIntegration = new apigateway.LambdaIntegration(
+      createItemFunction
+    );
+    rootResource.addResource("create").addMethod("POST", createIntegration);
   }
 }
