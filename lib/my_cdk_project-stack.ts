69fe38d63895ff136ebfbb795139b271655f7b1c
Abdullah Al Mahfuj <53569184+mahfuj-cse@users.noreply.github.com>
2023-11-08 18:09:53 +0600

    lambda function ans s3 bucket added

--- lib/my_cdk_project-stack.ts
+++ lib/my_cdk_project-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new lambda.Function(this, 'lambdaFunction', {
+      functionName: 'first-cdk-lambda',
+      code: new lambda.AssetCode('src'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 128
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyCdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'FirstBucket', {
+      versioned: true
+    });
   }
 }
