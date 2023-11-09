e1c9efb4c3d08396ee6f5e2d45e8ed5bd0c9b7d2
Abdullah Al Mahfuj <53569184+mahfuj-cse@users.noreply.github.com>
2023-11-09 16:11:28 +0600

    create an RDS instance

--- lib/my_cdk_project-stack.ts
+++ lib/my_cdk_project-stack.ts
@@ -3,10 +3,45 @@ import * as constructs from "constructs";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as kms from "aws-cdk-lib/aws-kms";
 
 export class MyCdkProjectStack extends cdk.Stack {
   constructor(scope: constructs.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "MyVPC", {
+      maxAzs: 2, // Number of availability zones
+      subnetConfiguration: [
+        {
+          cidrMask: 24, // Subnet size (adjust as needed)
+          name: "PublicSubnet",
+          subnetType: ec2.SubnetType.PUBLIC, // Public subnet
+        },
+        {
+          cidrMask: 24, // Subnet size (adjust as needed)
+          name: "PrivateSubnet",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT, // Private subnet with NAT
+        },
+      ],
+    });
+
+    const myKey = new kms.Key(this, "MyKey");
+    const postgresEngine = rds.DatabaseInstanceEngine.postgres({
+      version: rds.PostgresEngineVersion.VER_15_2, // Adjust the version as needed
+    });
+
+    new rds.DatabaseInstance(this, "InstanceWithCustomizedSecret", {
+      engine: postgresEngine, // Use the PostgreSQL engine
+      vpc,
+      credentials: rds.Credentials.fromGeneratedSecret("postgres", {
+        secretName: "my-cool-name",
+        encryptionKey: myKey,
+        excludeCharacters: "!&*^#@()",
+        replicaRegions: [{ region: "eu-west-1" }, { region: "eu-west-2" }],
+      }),
+    });
 
     // DynamoDB Table
     const dynamoTable = new dynamodb.Table(this, "MyDynamoDBTable", {
