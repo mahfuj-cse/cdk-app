cfd4311e8c4aa5fcf4f34695fa89195d1effb8ad
Abdullah Al Mahfuj <53569184+mahfuj-cse@users.noreply.github.com>
2023-11-12 01:04:13 +0600

    crud

--- lib/my_cdk_project-stack.ts
+++ lib/my_cdk_project-stack.ts
@@ -1,50 +1,58 @@
 import * as cdk from "aws-cdk-lib";
 import * as constructs from "constructs";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
-import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class MyCdkProjectStack extends cdk.Stack {
   constructor(scope: constructs.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // DynamoDB Table
-    const dynamoTable = new dynamodb.Table(this, "MyDynamoDBTable", {
-      partitionKey: { name: "itemId", type: dynamodb.AttributeType.STRING },
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // Adjust as needed
-      billingMode: dynamodb.BillingMode.PROVISIONED, // Use Provisioned billing mode for Free Tier
-      readCapacity: 5, // Adjust based on your expected usage within Free Tier limits
-      writeCapacity: 5, // Adjust based on your expected usage within Free Tier limits
-    });
-
     // Lambda Functions
-    const getAllItemsFunction = new lambda.Function(
+    const createProductFunction = new lambda.Function(
       this,
-      "getAllItemsFunction",
+      "CreateProductFunction",
       {
         runtime: lambda.Runtime.NODEJS_16_X,
-        handler: "get-all.handler",
-        code: lambda.Code.fromAsset("src"), // Replace with the correct code directory
-        environment: {
-          TABLE_NAME: dynamoTable.tableName,
-          PRIMARY_KEY: "itemId",
-        },
+        handler: "src/handlers.createProduct",
+        code: lambda.Code.fromAsset("src"),
       }
     );
 
-    const createItemFunction = new lambda.Function(this, "createItemFunction", {
+    const getProductFunction = new lambda.Function(this, "GetProductFunction", {
       runtime: lambda.Runtime.NODEJS_16_X,
-      handler: "create.handler",
-      code: lambda.Code.fromAsset("src"), // Replace with the correct code directory
-      environment: {
-        TABLE_NAME: dynamoTable.tableName,
-        PRIMARY_KEY: "itemId",
-      },
+      handler: "src/handlers.getProduct",
+      code: lambda.Code.fromAsset("src"),
     });
 
-    // Grant Permissions
-    dynamoTable.grantReadWriteData(createItemFunction);
-    dynamoTable.grantReadData(getAllItemsFunction);
+    const updateProductFunction = new lambda.Function(
+      this,
+      "UpdateProductFunction",
+      {
+        runtime: lambda.Runtime.NODEJS_16_X,
+        handler: "src/handlers.updateProduct",
+        code: lambda.Code.fromAsset("src"),
+      }
+    );
+
+    const deleteProductFunction = new lambda.Function(
+      this,
+      "DeleteProductFunction",
+      {
+        runtime: lambda.Runtime.NODEJS_16_X,
+        handler: "src/handlers.deleteProduct",
+        code: lambda.Code.fromAsset("src"),
+      }
+    );
+
+    const listProductFunction = new lambda.Function(
+      this,
+      "ListProductFunction",
+      {
+        runtime: lambda.Runtime.NODEJS_16_X,
+        handler: "src/handlers.listProduct",
+        code: lambda.Code.fromAsset("src"),
+      }
+    );
 
     // API Gateway
     const api = new apigateway.RestApi(this, "MyApi", {
@@ -54,14 +62,31 @@ export class MyCdkProjectStack extends cdk.Stack {
     const rootResource = api.root;
 
     // Integration with Lambda Functions
-    const getAllIntegration = new apigateway.LambdaIntegration(
-      getAllItemsFunction
-    );
+    const getAllIntegration = new apigateway.LambdaIntegration(listProductFunction);
     rootResource.addMethod("GET", getAllIntegration);
 
-    const createIntegration = new apigateway.LambdaIntegration(
-      createItemFunction
-    );
-    rootResource.addResource("create").addMethod("POST", createIntegration);
+    // Create resource for individual product
+    const productResource = rootResource.addResource("product");
+
+    // POST - /product
+    const createProductIntegration = new apigateway.LambdaIntegration(createProductFunction);
+    productResource.addMethod("POST", createProductIntegration);
+
+    // GET, PUT, DELETE - /product/{id}
+    const productIdResource = productResource.addResource("{id}");
+
+    const getProductIntegration = new apigateway.LambdaIntegration(getProductFunction);
+    productIdResource.addMethod("GET", getProductIntegration);
+
+    const updateProductIntegration = new apigateway.LambdaIntegration(updateProductFunction);
+    productIdResource.addMethod("PUT", updateProductIntegration);
+
+    const deleteProductIntegration = new apigateway.LambdaIntegration(deleteProductFunction);
+    productIdResource.addMethod("DELETE", deleteProductIntegration);
+
+    // GET - /products
+    const listProductsIntegration = new apigateway.LambdaIntegration(listProductFunction);
+    const productsResource = rootResource.addResource("products");
+    productsResource.addMethod("GET", listProductsIntegration);
   }
 }
