9f268520012cbf56b9eacdedd26c47c532bea5eb
Abdullah Al Mahfuj <53569184+mahfuj-cse@users.noreply.github.com>
2023-11-14 11:35:25 +0600

    multi stack add

--- lib/my_cdk_project-stack.ts
+++ lib/my_cdk_project-stack.ts
@@ -1,92 +1,111 @@
 import * as cdk from "aws-cdk-lib";
-import * as constructs from "constructs";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
+import * as iam from "aws-cdk-lib/aws-iam";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as cr from "aws-cdk-lib/custom-resources";
 
-export class MyCdkProjectStack extends cdk.Stack {
-  constructor(scope: constructs.Construct, id: string, props?: cdk.StackProps) {
+export class RDSStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Lambda Functions
-    const createProductFunction = new lambda.Function(
-      this,
-      "CreateProductFunction",
-      {
-        runtime: lambda.Runtime.NODEJS_16_X,
-        handler: "src/handlers.createProduct",
-        code: lambda.Code.fromAsset("src"),
-      }
-    );
-
-    const getProductFunction = new lambda.Function(this, "GetProductFunction", {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      handler: "src/handlers.getProduct",
-      code: lambda.Code.fromAsset("src"),
+    // VPC
+    const vpc = new ec2.Vpc(this, "MyVPC", {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "PublicSubnet",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "PrivateSubnet",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
     });
 
-    const updateProductFunction = new lambda.Function(
-      this,
-      "UpdateProductFunction",
-      {
-        runtime: lambda.Runtime.NODEJS_16_X,
-        handler: "src/handlers.updateProduct",
-        code: lambda.Code.fromAsset("src"),
-      }
-    );
-
-    const deleteProductFunction = new lambda.Function(
-      this,
-      "DeleteProductFunction",
-      {
-        runtime: lambda.Runtime.NODEJS_16_X,
-        handler: "src/handlers.deleteProduct",
-        code: lambda.Code.fromAsset("src"),
-      }
-    );
-
-    const listProductFunction = new lambda.Function(
-      this,
-      "ListProductFunction",
-      {
-        runtime: lambda.Runtime.NODEJS_16_X,
-        handler: "src/handlers.listProduct",
-        code: lambda.Code.fromAsset("src"),
-      }
-    );
-
-    // API Gateway
-    const api = new apigateway.RestApi(this, "MyApi", {
-      restApiName: "My API",
+    const adminSecret = new secretsmanager.Secret(this, "MyAdminSecret", {
+      secretName: "admin-secret",
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({
+          username: "your-master-username",
+        }),
+        excludePunctuation: true,
+        includeSpace: false,
+        generateStringKey: "password",
+      },
     });
 
-    const rootResource = api.root;
 
-    // Integration with Lambda Functions
-    const getAllIntegration = new apigateway.LambdaIntegration(listProductFunction);
-    rootResource.addMethod("GET", getAllIntegration);
+    
+    // RDS Instance
+    const rdsInstance = new rds.DatabaseInstance(this, "MyRDSInstance", {
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_13,
+      }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      vpc,
+      credentials: rds.Credentials.fromGeneratedSecret("adminSecret"),
+    });
 
-    // Create resource for individual product
-    const productResource = rootResource.addResource("product");
+    // IAM Role
+    const role = new iam.Role(this, "MyRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
 
-    // POST - /product
-    const createProductIntegration = new apigateway.LambdaIntegration(createProductFunction);
-    productResource.addMethod("POST", createProductIntegration);
+    // Grant permissions to the RDS instance secret
+    if (rdsInstance.secret) {
+      rdsInstance.secret.grantRead(role);
+    } else {
+      console.error("rdsInstance.secret is not set. Check your RDS instance configuration.");
+    }
 
-    // GET, PUT, DELETE - /product/{id}
-    const productIdResource = productResource.addResource("{id}");
+    // Lambda function
+    const instantiateFunction = new lambda.Function(this, "InstantiateFunction", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("src"), // Assuming your Lambda code is in a 'src' directory
+      role,
+      environment: {
+        RDS_SECRET_ARN: rdsInstance.secret?.secretArn || "", // Pass the secret ARN to Lambda
+        DB_NAME: "mydatabase",
+        TABLE_NAME: "products",
+      },
+    });
 
-    const getProductIntegration = new apigateway.LambdaIntegration(getProductFunction);
-    productIdResource.addMethod("GET", getProductIntegration);
+    // Use the AWS SDK to get the Lambda function ARN
+    const lambdaFunctionArn = instantiateFunction.functionArn;
 
-    const updateProductIntegration = new apigateway.LambdaIntegration(updateProductFunction);
-    productIdResource.addMethod("PUT", updateProductIntegration);
+    // Attach a policy to grant Lambda InvokeFunction permission
+    role.attachInlinePolicy(
+      new iam.Policy(this, 'LambdaInvokePolicy', {
+        statements: [
+          new iam.PolicyStatement({
+            actions: ['lambda:InvokeFunction'],
+            effect: iam.Effect.ALLOW,
+            resources: [lambdaFunctionArn],
+          }),
+        ],
+      })
+    );
 
-    const deleteProductIntegration = new apigateway.LambdaIntegration(deleteProductFunction);
-    productIdResource.addMethod("DELETE", deleteProductIntegration);
-
-    // GET - /products
-    const listProductsIntegration = new apigateway.LambdaIntegration(listProductFunction);
-    const productsResource = rootResource.addResource("products");
-    productsResource.addMethod("GET", listProductsIntegration);
+    // Trigger Lambda function with a custom resource
+    new cr.AwsCustomResource(this, "TriggerInstantiate", {
+      onUpdate: {
+        service: "Lambda",
+        action: "invoke",
+        physicalResourceId: cr.PhysicalResourceId.of("TriggerInstantiate"),
+        parameters: {
+          FunctionName: instantiateFunction.functionName,
+        },
+      },
+      policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [lambdaFunctionArn],
+      }),
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,47 @@
-export const handler = async (event: { name: string }) => {
-    const result: string = event.name ? `Good Job ${event.name}!` : 'Good Job'
-    return result
-}
+import { Handler } from 'aws-lambda';
+import { SecretsManager } from 'aws-sdk';
+import { Client } from 'pg';
+
+const RDS_SECRET_ARN = process.env.RDS_SECRET_ARN!;
+const DB_NAME = process.env.DB_NAME!;
+const TABLE_NAME = process.env.TABLE_NAME!;
+
+const secrets = new SecretsManager();
+
+export const handler: Handler = async () => {
+    try {
+      // Retrieve RDS Admin credentials
+      const adminSecret = await secrets.getSecretValue({ SecretId: RDS_SECRET_ARN }).promise();
+      const admin = JSON.parse(adminSecret.SecretString as string);
+
+      // Instantiate RDS Client with Admin
+      const client = new Client({
+        host: admin.host,
+        user: admin.username,
+        password: admin.password,
+        database: 'postgres',
+        port: 5432,
+      });
+
+      // Connect to RDS instance with Admin
+      await client.connect();
+      console.log('Setting up a new database...');
+      await client.query(`CREATE DATABASE ${DB_NAME};`);
+      await client.query(`\\c ${DB_NAME}`);
+      console.log('Setting up a new table...');
+      const createTableCommand = `
+        CREATE TABLE ${TABLE_NAME} (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255),
+          description VARCHAR(1000),
+          price DECIMAL(10, 2)
+        );`;
+      await client.query(createTableCommand);
+      console.log('Setup completed!');
+      await client.end();
+
+    } catch (error) {
+      console.error('Error creating database:', error);
+      throw error;
+    }
+};
