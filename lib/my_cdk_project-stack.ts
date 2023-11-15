8f39f5278bdd14f8014ee3e576dbcaa14f499e2f
Abdullah Al Mahfuj <53569184+mahfuj-cse@users.noreply.github.com>
2023-11-15 21:19:37 +0600

    book api added

--- lib/my_cdk_project-stack.ts
+++ lib/my_cdk_project-stack.ts
@@ -1,118 +1,128 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as rds from 'aws-cdk-lib/aws-rds';
-import * as secrets from 'aws-cdk-lib/aws-secretsmanager';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as cr from 'aws-cdk-lib/custom-resources';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as secrets from "aws-cdk-lib/aws-secretsmanager";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as nodejs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as cr from "aws-cdk-lib/custom-resources";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class CdkTsRdsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // VPC for RDS and Lambda resolvers
-    const vpc = new ec2.Vpc(this, 'VPC', {
-      vpcName: 'rds-vpc',
+    const vpc = new ec2.Vpc(this, "VPC", {
+      vpcName: "rds-vpc",
       maxAzs: 2,
       natGateways: 1,
       subnetConfiguration: [
         {
           subnetType: ec2.SubnetType.PUBLIC, // Add a public subnet for internet access
           cidrMask: 24,
-          name: 'public',
+          name: "public",
         },
         {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
           cidrMask: 24,
-          name: 'rds',
-        }
-      ]
+          name: "rds",
+        },
+      ],
     });
 
     // Security Groups
-    const securityGroupResolvers = new ec2.SecurityGroup(this, 'SecurityGroupResolvers', {
-      vpc,
-      securityGroupName: 'resolvers-sg',
-      description: 'Security Group with Resolvers',
-    });
+    const securityGroupResolvers = new ec2.SecurityGroup(
+      this,
+      "SecurityGroupResolvers",
+      {
+        vpc,
+        securityGroupName: "resolvers-sg",
+        description: "Security Group with Resolvers",
+      }
+    );
 
-    const securityGroupRds = new ec2.SecurityGroup(this, 'SecurityGroupRds', {
+    const securityGroupRds = new ec2.SecurityGroup(this, "SecurityGroupRds", {
       vpc,
-      securityGroupName: 'rds-sg',
-      description: 'Security Group with RDS',
+      securityGroupName: "rds-sg",
+      description: "Security Group with RDS",
     });
 
     // Ingress and Egress Rules
     securityGroupRds.addIngressRule(
       securityGroupResolvers,
       ec2.Port.tcp(5432),
-      'Allow inbound traffic to RDS'
+      "Allow inbound traffic to RDS"
     );
-    
+
     // VPC Interfaces
-    vpc.addInterfaceEndpoint('LAMBDA', {
+    vpc.addInterfaceEndpoint("LAMBDA", {
       service: ec2.InterfaceVpcEndpointAwsService.LAMBDA,
       subnets: { subnets: vpc.isolatedSubnets },
       securityGroups: [securityGroupResolvers],
     });
 
-    vpc.addInterfaceEndpoint('SECRETS_MANAGER', {
+    vpc.addInterfaceEndpoint("SECRETS_MANAGER", {
       service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
       subnets: { subnets: vpc.isolatedSubnets },
       securityGroups: [securityGroupResolvers],
-    });    
+    });
 
     // IAM Role
-    const role = new iam.Role(this, 'Role', {
-      roleName: 'rds-role',
-      description: 'Role used in the RDS stack',
+    const role = new iam.Role(this, "Role", {
+      roleName: "rds-role",
+      description: "Role used in the RDS stack",
       assumedBy: new iam.CompositePrincipal(
-        new iam.ServicePrincipal('ec2.amazonaws.com'),
-        new iam.ServicePrincipal('lambda.amazonaws.com'),
-      )
+        new iam.ServicePrincipal("ec2.amazonaws.com"),
+        new iam.ServicePrincipal("lambda.amazonaws.com")
+      ),
     });
 
     role.addToPolicy(
       new iam.PolicyStatement({
         effect: iam.Effect.ALLOW,
         actions: [
-          'cloudwatch:PutMetricData',
-          'ec2:CreateNetworkInterface',
-          'ec2:DescribeNetworkInterfaces',
-          'ec2:DeleteNetworkInterface',
-          'ec2:DescribeInstances',
-          'ec2:DescribeSubnets',
-          'ec2:DescribeSecurityGroups',
-          'ec2:DescribeRouteTables',
-          'logs:CreateLogGroup',
-          'logs:CreateLogStream',
-          'logs:PutLogEvents',
-          'lambda:InvokeFunction',
-          'secretsmanager:GetSecretValue',
-          'kms:decrypt',
-          'rds-db:connect',
+          "cloudwatch:PutMetricData",
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeRouteTables",
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "lambda:InvokeFunction",
+          "secretsmanager:GetSecretValue",
+          "kms:decrypt",
+          "rds-db:connect",
         ],
-        resources: ['*'],
+        resources: ["*"],
       })
     );
 
     // RDS PostgreSQL Instance
-    const rdsInstance = new rds.DatabaseInstance(this, 'PostgresRds', {
+    const rdsInstance = new rds.DatabaseInstance(this, "PostgresRds", {
       vpc,
       securityGroups: [securityGroupRds],
       vpcSubnets: { subnets: vpc.isolatedSubnets },
       availabilityZone: vpc.isolatedSubnets[0].availabilityZone,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.SMALL),
-      engine: rds.DatabaseInstanceEngine.postgres({version: rds.PostgresEngineVersion.VER_14_6}),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T4G,
+        ec2.InstanceSize.SMALL
+      ),
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_14_6,
+      }),
       port: 5432,
-      instanceIdentifier: 'new-librarydb-instance',
+      instanceIdentifier: "new-librarydb-instance",
       allocatedStorage: 10,
       maxAllocatedStorage: 10,
       deleteAutomatedBackups: true,
       backupRetention: cdk.Duration.millis(0),
-      credentials: rds.Credentials.fromUsername('libraryadmin'),
+      credentials: rds.Credentials.fromUsername("libraryadmin"),
       publiclyAccessible: true,
       databaseName: "libraryDatabase",
     });
@@ -122,20 +132,20 @@ export class CdkTsRdsStack extends cdk.Stack {
     // Secrets for database credentials.
     const credentials = secrets.Secret.fromSecretCompleteArn(
       this,
-      'CredentialsSecret',
-      'arn:aws:secretsmanager:us-east-1:253399877957:secret:rds-db-creds-W8vvei'
+      "CredentialsSecret",
+      "arn:aws:secretsmanager:us-east-1:253399877957:secret:rds-db-creds-W8vvei"
     );
     credentials.grantRead(role);
 
     // Returns function to connect with the RDS instance.
-    const createResolver = (name: string, entry: string) =>
+    const createLambdaFunction = (name: string, handler: string) =>
       new nodejs.NodejsFunction(this, name, {
         functionName: name,
-        entry: entry,
+        entry: `src/${handler}.ts`,
         bundling: {
-          externalModules: ['pg-native'],
+          externalModules: ["pg-native"],
         },
-        runtime: lambda.Runtime.NODEJS_18_X,
+        runtime: lambda.Runtime.NODEJS_16_X,
         timeout: cdk.Duration.minutes(2),
         role,
         vpc,
@@ -149,33 +159,60 @@ export class CdkTsRdsStack extends cdk.Stack {
       });
 
     // Instantiate a new db with user and permissions and also add a table.
-    const instantiate = createResolver('instantiate', 'src/instantiate.ts');
+    const instantiate = createLambdaFunction("instantiate", "instantiate");
+
     instantiate.node.addDependency(rdsInstance);
 
-    // Lambda function for adding a book in the RDS table.
-    const addBook = createResolver('add-book', 'src/addBook.ts');
-    addBook.node.addDependency(rdsInstance);
+    // Updated addBookFunction
+    const addBookFunction = createLambdaFunction(
+      "AddBookFunction",
+      "addBook"
+    );
+    addBookFunction.node.addDependency(rdsInstance);
 
-    // Lambda function for getting books in the RDS table.
-    const getBooks = createResolver('get-books', 'src/getBooks.ts');
-    getBooks.node.addDependency(rdsInstance);
+    // Updated getBooksFunction
+    const getBooksFunction = createLambdaFunction(
+      "GetBooksFunction",
+      "getBooks"
+    );
+    getBooksFunction.node.addDependency(rdsInstance);
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, "BookApi", {
+      restApiName: "Book API",
+    });
+
+    // /book endpoint for addBook Lambda
+    const bookResource = api.root.addResource("book");
+    bookResource.addMethod(
+      "POST",
+      new apigateway.LambdaIntegration(addBookFunction)
+    );
+    bookResource.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(getBooksFunction)
+    );
 
     // Custom Resource to execute the instantiate function.
-    const customResource = new cr.AwsCustomResource(this, 'TriggerInstantiate', {
-      functionName: 'trigger-instantiate',
-      role,
-      onUpdate: {
-        service: 'Lambda',
-        action: 'invoke',
-        parameters: {
-          FunctionName: instantiate.functionName,
+    const customResource = new cr.AwsCustomResource(
+      this,
+      "TriggerInstantiate",
+      {
+        functionName: "trigger-instantiate",
+        role,
+        onUpdate: {
+          service: "Lambda",
+          action: "invoke",
+          parameters: {
+            FunctionName: instantiate.functionName,
+          },
+          physicalResourceId: cr.PhysicalResourceId.of("TriggerInstantiate"),
         },
-        physicalResourceId: cr.PhysicalResourceId.of('TriggerInstantiate'),
-      },
-      policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
-        resources: [instantiate.functionArn],
-      }),
-    });
+        policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
+          resources: [instantiate.functionArn],
+        }),
+      }
+    );
     customResource.node.addDependency(instantiate);
   }
 }
